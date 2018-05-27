9016619780f592b4401760ccc57acbd6858bf482
Hebe <doveskila@gmail.com>
2018-05-28 01:15:54 +0800

    sort

--- src/main/java/sort/CocktailSort.java
+++ src/main/java/sort/CocktailSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * 鸡尾酒排序 冒泡改进
  * @author Hebe Han
@@ -8,16 +10,33 @@ package sort;
 public class CocktailSort {
 
     public static void main(String[] args) {
-
+        cocktailSort(new int[]{3,7,6,9,0,3,6,2,1});
     }
 
     public static void cocktailSort(int[] numbs){
         int left = 0;
-        int right = numbs.length-1;
-        while (left > right){
-            for (int i = 0; i < numbs.length; i++) {
-                
+        int right = numbs.length;
+        while (right > left){
+
+            for (int i = left; i < right; i++) {
+//                System.out.println("left = "+left);
+                int tmp = numbs[i];
+                if (tmp< numbs[left]){
+                    numbs[i] = numbs[left];
+                    numbs[left] = tmp;
+                }
             }
+            right--;
+            for (int i = right; i >left; i--) {
+//                System.out.println("right = "+right);
+                int tmp = numbs[i];
+                if (tmp > numbs[right]){
+                    numbs[i] = numbs[right];
+                    numbs[right] = tmp;
+                }
+            }
+            left++;
         }
+//        System.out.println(JSON.toJSON(numbs));
     }
 }
